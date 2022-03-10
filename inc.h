a73151417f393a87bc4d502ada184e5f7cbce6e3
桂林哈秋 <glhaqiu@sina.com>
2022-03-10 22:03:24 +0800

    Update inc.h

--- inc.h
+++ inc.h
@@ -4,3 +4,9 @@ int count_index = 1;
 
 #define printLn(s) { std::cout << "\n" << "line_" << count_index << ": " << s << "\n"; \
  count_index++; }
+
+template<typename T>
+void showTypeSize()
+{
+  printLn(sizeof(T));
+}
